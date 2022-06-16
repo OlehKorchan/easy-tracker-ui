3915e1917bd69cbe789698430a9dca1f23c958af
Oleh Korchan <oleh_korchan@epam.com>
2022-06-16 12:27:04 +0300

    Change from template to reactive form (income)

--- src/app/modules/income/components/income-form/income-form.component.ts
+++ src/app/modules/income/components/income-form/income-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IIncomeCreateRequest } from '../../models/income-create.request';
 import { MatDialogRef } from '@angular/material/dialog';
 import { IncomeService } from '../../services/income.service';
@@ -6,18 +6,20 @@ import { Subscription } from 'rxjs';
 import { CurrencyCodes } from 'src/app/shared/models/currency-codes';
 import { IIncomeResponse } from '../../models/income-response';
 import { UserService } from 'src/app/modules/user-statistics/services/user.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './income-form.component.html',
   styleUrls: ['./income-form.component.css'],
 })
-export class IncomeFormComponent implements OnDestroy {
-  incomeCreateRequest: IIncomeCreateRequest = {
-    amount: 0,
-    comment: '',
-    currency: CurrencyCodes.USD,
-  };
-  incomeCreateResponse: IIncomeResponse = {
+export class IncomeFormComponent implements OnInit, OnDestroy {
+  private incomeResponseSub$: Subscription | undefined;
+
+  public incomeForm!: FormGroup;
+  public amountControl!: FormControl;
+  public commentControl!: FormControl;
+  public currencyControl!: FormControl;
+  public incomeCreateResponse: IIncomeResponse = {
     id: '',
     amount: 0,
     comment: '',
@@ -25,8 +27,7 @@ export class IncomeFormComponent implements OnDestroy {
     errors: [],
     currency: CurrencyCodes.USD,
   };
-  incomeResponseSub$: Subscription | undefined;
-  currencies: string[];
+  public currencies: string[];
 
   constructor(
     public dialogRef: MatDialogRef<IncomeFormComponent>,
@@ -36,9 +37,15 @@ export class IncomeFormComponent implements OnDestroy {
     this.currencies = userService.getCurrenciesList();
   }
 
-  onSubmit() {
+  public onSubmit() {
+    const incomeRequest: IIncomeCreateRequest = {
+      amount: this.amountControl.value,
+      comment: this.commentControl.value,
+      currency: this.currencyControl.value,
+    };
+
     this.incomeResponseSub$ = this.incomeService
-      .addIncome(this.incomeCreateRequest)
+      .addIncome(incomeRequest)
       .subscribe({
         next: (data) => {
           if (!data.errors.length) {
@@ -47,13 +54,29 @@ export class IncomeFormComponent implements OnDestroy {
             this.incomeCreateResponse = data;
           }
         },
-        error: (error) => {
-          console.log(error?.error?.errorMessage);
-        },
+        error: () => this.dialogRef.close(),
       });
   }
 
-  ngOnDestroy(): void {
+  public ngOnInit(): void {
+    this.amountControl = new FormControl(Number, [
+      Validators.required,
+      Validators.min(0.01),
+      Validators.max(1000000000),
+    ]);
+    this.commentControl = new FormControl('', [Validators.maxLength(300)]);
+    this.currencyControl = new FormControl(CurrencyCodes.USD, [
+      Validators.required,
+    ]);
+
+    this.incomeForm = new FormGroup({
+      amount: this.amountControl,
+      comment: this.commentControl,
+      currency: this.currencyControl,
+    });
+  }
+
+  public ngOnDestroy(): void {
     this.incomeResponseSub$?.unsubscribe();
   }
 }
